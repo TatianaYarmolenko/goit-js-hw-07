0d0e3321f5523d9f13657b4b34697efc32cdb01b
ta-moli <yarnusha7@gmail.com>
2021-08-02 23:42:12 +0300

    работа с 1 задачей

---
+++ index.js
@@ -0,0 +1,7 @@
+import './js/task-1.js';
+import './js/task-2.js';
+import './js/task-3.js';
+import './js/task-4.js';
+import './js/task-5.js';
+import './js/task-6.js';
+import './js/task-7.js';
---
+++ js/task-1.js
@@ -0,0 +1,29 @@
+const elemCategories = document.querySelector('ul#categories');
+console.log('elemCategories', elemCategories);
+console.log(elemCategories.textContent);
+
+// const firstElemCategories = elemCategories.firstElementChild;
+// console.log(firstElemCategories);
+// console.log(elemCategories.lastElementChild);
+
+
+
+
+
+
+// const elemCategories = document.querySelector('#categories');
+// console.log(elemCategories);
+// const elemItems = elemCategories.querySelectorAll('.item');
+// console.log(`В списке ${elemItems.length} категории`);
+// console.log('');
+// //console.log(elemItems);
+// const titles = [];
+// elemItems.forEach(elem => {
+//   console.log('Категория : ', elem.getElementsByTagName('h2')[0].textContent);
+//   console.log(
+//     'Количество эелментов : ',
+//     elem.getElementsByTagName('li').length
+//   );
+//   console.log('');
+// });
+//console.log(title.flatMap(elem => Object.values(elem)));
