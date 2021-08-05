e711720f620c9f8a9ed76bf893d55709563a4de5
ta-moli <yarnusha7@gmail.com>
2021-08-05 23:23:20 +0300

    add in html tasks

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import './js/task-1.js';
-import './js/task-2.js';
-import './js/task-3.js';
-import './js/task-4.js';
-import './js/task-5.js';
-import './js/task-6.js';
-import './js/task-7.js';
+import './js/task-01.js';
+import './js/task-02.js';
+import './js/task-03.js';
+import './js/task-04.js';
+import './js/task-05.js';
+import './js/task-06.js';
+import './js/task-07.js';
