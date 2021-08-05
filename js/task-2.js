7ce27bb5d8e9535cf17884b30a5d1ef491d7013f
ta-moli <yarnusha7@gmail.com>
2021-08-05 22:40:20 +0300

    add html tasks

--- js/task-2.js
+++ js/task-2.js
@@ -1,24 +1,12 @@
-const ingredients = [
-   'Картошка',
-   'Грибы',
-   'Чеснок',
-   'Помидоры',
-   'Зелень',
-   'Приправы',
- ];
+const ingredients = ['Картошка', 'Грибы', 'Чеснок', 'Помидоры', 'Зелень', 'Приправы'];
 
-// const elIngr = document.querySelector('#ingredients');
-//  elemIngredients.append('ingredients');
-//  console.log(elIngr);
-//  console.log(elemIngredients);
-//  document.ul.ingredients.firstChild(elemIngredients);
+const elemIngredients = ingredients.map(ingredient => {
+  const elemIngredient = document.createElement('li');
+  elemIngredient.textContent = ingredient;
 
-const elemIngredientsParent = document.getElementById('ingredients')
-console.log(elemIngredientsParent);
+  return elemIngredient;
+});
 
-const elemIngredients = document.createElement('li')
-elemIngredients.textContent = 'Картошка'
-// elemIngredients.classList.add('Картошка')
-// console.log(elemIngredients);
-
-elemIngredientsParent.prepend(elemIngredients)
+const listIngredients = document.querySelector('#ingredients');
+listIngredients.append(...elemIngredients);
+console.log(listIngredients);
